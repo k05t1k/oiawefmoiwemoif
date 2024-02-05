8c05448040f1c03298a93196fd628561e741e1e4
k05t1k <isip_k.m.fionov@mpt.ru>
2024-02-05 15:01:15 +0300

    kkkk

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -21,17 +23,24 @@ namespace WpfApp1
     public partial class MainWindow : Window
     {
         Button[] buttons;
+        Label[] label;
         Random random = new Random();
+
+        public int winlose = 0;
 
         public MainWindow()
         {
             InitializeComponent();
-            buttons = new Button[9] {_1, _2, _3, _4, _5, _6, _7, _8, _9 };
+            buttons = new Button[9] { _1, _2, _3, _4, _5, _6, _7, _8, _9 };
+            label = new Label[1] { _1337 };
+
+            for (int i = 0; i < 9; i++)
+                buttons[i].IsEnabled = false;
         }
 
         int[,] winner_data = new int[8, 3]
-        {
-            { 0, 1, 2 }, 
+{
+            { 0, 1, 2 },
             { 3, 4, 5 },
             { 6, 7, 8 },
 
@@ -41,16 +50,70 @@ namespace WpfApp1
             { 0, 3, 6 },
             { 1, 4, 7 },
             { 2, 5, 8 },
-        }; 
+};
+
+        public int round = 1;
+
+        public int win = 0;
+        public int tie = 0;
+        public int lose = 0;
+
+        public string player = "X";
+        public string enemy = "0";
+
+        public bool WinLose()
+        {
+            int flag = 0;
+            for (int i = 0; i < winner_data.GetLength(0); i++)
+            {
+                for (int k = 0; k < winner_data.GetLength(1); k++)
+                {
+                    if (buttons[winner_data[i, k]].Content == player &&
+                        buttons[winner_data[i, k + 1]].Content == player &&
+                        buttons[winner_data[i, k + 2]].Content == player)
+                    {
+                        win++;
+                        label[winlose].Content = "Вы выиграли!";
+                        return true;
+                    }
+                    else if (buttons[winner_data[i, k]].Content == enemy &&
+                        buttons[winner_data[i, k + 1]].Content == enemy &&
+                        buttons[winner_data[i, k + 2]].Content == enemy)
+                    {
+                        lose++;
+                        label[winlose].Content = "Вы проиграли!";
+                        return true;
+                    }
+
+                    break;
+                }
+            }
+
+            for (int i = 0; i < 9; i++)
+            {
+                if (buttons[i].IsEnabled == false)
+                    flag++;
+
+                if (flag == 9)
+                {
+                    tie++;
+                    label[winlose].Content = "Ничья!";
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        int key_value = 0;
 
         private void _1_Click(object sender, RoutedEventArgs e)
         {
-            int flag = 0;
-
             int key = random.Next(0, 9);
 
-            (sender as Button).Content = "X";
+            (sender as Button).Content = player;
             (sender as Button).IsEnabled = false;
+
             for (int i = 0; i < 9; i++)
             {
                 if (buttons[key].IsEnabled == false)
@@ -58,40 +121,59 @@ namespace WpfApp1
                     key = random.Next(0, 9);
                     continue;
                 }
-                flag++;
+
+                buttons[key].Content = enemy;
+                buttons[key].IsEnabled = false;
+                // label[0].Content = $"set on {i}";
                 break;
             }
-            if (flag < 9)
-            {
-                buttons[key].Content = "0";
-                buttons[key].IsEnabled = false;
-            }
 
-            for (int i = 0; i < winner_data.GetLength(0);  i++)
+            if (WinLose())
             {
-                for (int k = 0; k < winner_data.GetLength(1); k++)
+                Start.IsEnabled = true;
+
+                key = random.Next(0, 9);
+
+                for (int j = 0; j < 9; j++)
                 {
-                    if (buttons[winner_data[i, k]].Content == "X" &&
-                        buttons[winner_data[i, k + 1]].Content == "X" &&
-                        buttons[winner_data[i, k + 2]].Content == "X")
+                    if (buttons[key].Content == "X" && round % 2 != 0)
                     {
-                        MessageBox.Show("You win!");
-                        this.Close();
-                        return;
-                    }
-                    else if (buttons[winner_data[i, k]].Content == "0" &&
-                        buttons[winner_data[i, k + 1]].Content == "0" &&
-                        buttons[winner_data[i, k + 2]].Content == "0")
-                    {
-                        MessageBox.Show("You lose :(");
-                        this.Close();
-                        return;
+                        key_value = key;
+                        key = 0;
+                        buttons[j].IsEnabled = false;
+                        continue;
                     }
 
-                    break;
+                    key = random.Next(0, 9);
+                    buttons[j].IsEnabled = false;
+                }
+
+                round++;
+
+                if (round % 2 != 0)
+                {
+                    player = "X";
+                    enemy = "0";
+                }
+                else
+                {
+                    player = "0";
+                    enemy = "X";
                 }
             }
         }
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            label[winlose].Content = " ";
+            for (int i = 0; i < 9; i++) 
+            {
+                if (i == key_value && round % 2 == 0)
+                    continue;
 
+                buttons[i].Content = " ";
+                buttons[i].IsEnabled = true; 
+            }
+            (sender as Button).IsEnabled = false;
+        }
     }
 }
